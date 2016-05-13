39bdade5b81b0ddd5fb98f831729768e545bdb2f
Yvonne Chan <ychan1352@gmail.com>
2016-05-13 12:19:20 -0400

    began work on settings

---
+++ Menu.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class Menu{
+    private Container editor;
+    private JTextField textbox;
+    private JLabel button;
+    
+    
+    
+}
