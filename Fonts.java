48ee11eb4168f829d4a7d848eb0ff89d5e5acad3
Sammy Almawaldi <almawaldi@hotmail.com>
2016-05-13 11:44:40 -0400

    Fun with Fonts

---
+++ Fonts.java
@@ -0,0 +1,9 @@
+import java.awt.Font;
+
+public class Fonts{
+
+    public static void main(String[] args){
+	Font f = new Font("Purisa", Font.PLAIN, 24);
+	System.out.println(f.toString());
+    }
+}
