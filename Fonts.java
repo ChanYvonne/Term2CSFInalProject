2cd3656f3de88fe5f921a524ae11ef91f107798e
Sammy Almawaldi <almawaldi@hotmail.com>
2016-05-13 12:25:04 -0400

    Delete Fonts.java

--- Fonts.java
+++
@@ -1,9 +0,0 @@
-import java.awt.Font;
-
-public class Fonts{
-
-    public static void main(String[] args){
-	Font f = new Font("Purisa", Font.PLAIN, 24);
-	System.out.println(f.toString());
-    }
-}
