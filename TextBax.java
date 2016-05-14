ccd9ad522f52e9d5e3f61fd076864921eddd7301
Sammy Almawaldi <almawaldi@hotmail.com>
2016-05-14 16:49:36 -0400

    Testing

---
+++ TextBax.java
@@ -0,0 +1,10 @@
+import javax.swing.JTextField;
+import javax.swing.JPanel;
+import java.awt.event;
+
+public class TextBax extends JPanel implements ActionListener{
+    
+    public static void main(String[] args){
+	JTextField textField = new JTextField(20);
+    }
+}
