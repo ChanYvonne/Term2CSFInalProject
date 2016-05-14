5921eba93b0aa1a71fa08c3dee4a4805ccb70dc9
Sammy Almawaldi <almawaldi@hotmail.com>
2016-05-14 17:00:58 -0400

    Complies and runs, but there isn't actually a gui

--- TextBax.java
+++ TextBax.java
@@ -1,10 +1,15 @@
 import javax.swing.JTextField;
 import javax.swing.JPanel;
-import java.awt.event;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class TextBax extends JPanel implements ActionListener{
     
     public static void main(String[] args){
-	JTextField textField = new JTextField(20);
+    	JTextField textField = new JTextField(20);
+    }
+
+    public void actionPerformed(ActionEvent e){
+    	System.out.println("An action was perfomed. Now do something about it.");
     }
 }
