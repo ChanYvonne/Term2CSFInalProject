3b45ae8ad9c56347f6cad585e61f87a5b773130a
Sammy Almawaldi <almawaldi@hotmail.com>
2016-05-14 17:23:40 -0400

    The textbox is displaying, just need to alter the size of the window upon opening

--- TextBax.java
+++ TextBax.java
@@ -2,14 +2,24 @@ import javax.swing.JTextField;
 import javax.swing.JPanel;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import javax.swing.JFrame;
+import java.awt.BorderLayout;
+
 
 public class TextBax extends JPanel implements ActionListener{
     
     public static void main(String[] args){
+    	JFrame frame = new JFrame("TextBax");
+    	JPanel p = new JPanel(new BorderLayout());
     	JTextField textField = new JTextField(20);
+    	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    	frame.setVisible(true);
+    	frame.add(textField);
     }
 
     public void actionPerformed(ActionEvent e){
     	System.out.println("An action was perfomed. Now do something about it.");
     }
+
+
 }
