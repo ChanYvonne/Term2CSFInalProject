c36e0418e9ad202de632282b8a1e578225421e88
Yvonne Chan <ychan1352@gmail.com>
2016-05-16 01:44:22 -0400

    added buttons, textbox, and alignment

--- Menu.java
+++ Menu.java
@@ -2,11 +2,92 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 
-public class Menu{
+//note for later: create our own icon(?)
+public class Menu extends JFrame implements ActionListener{
     private Container editor;
     private JTextField textbox;
-    private JLabel button;
+    private JButton bold,italic;
+    private JRadioButton lalign,center,ralign;
+    private ButtonGroup alignment;
     
+
+    public Menu(){
+	this.setTitle("Word Processor");
+	this.setSize(500,500);
+	this.setLocation(400,200);
+	this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+	editor = this.getContentPane();
+	editor.setLayout(new BoxLayout(editor, BoxLayout.PAGE_AXIS));
+
+        bold = new JButton("bold");
+	bold.addActionListener(this);
+	bold.setActionCommand("turnB");
+	bold.setMnemonic(KeyEvent.VK_B);
+	
+        italic = new JButton("italic");
+	italic.addActionListener(this);
+	italic.setActionCommand("turnI");
+	italic.setMnemonic(KeyEvent.VK_I);
+
+	alignment = new ButtonGroup();
+	
+	lalign = new JRadioButton("Left-aligned");
+	lalign.setActionCommand("Left-aligned");
+	lalign.addActionListener(this);
+	lalign.setSelected(true);
+	
+	center = new JRadioButton("Center");
+	center.setActionCommand("Center");
+	center.addActionListener(this);
+	
+	ralign = new JRadioButton("Right-aligned");
+	ralign.setActionCommand("Right-aligned");
+	ralign.addActionListener(this);
+
+	alignment.add(lalign);
+	alignment.add(center);
+	alignment.add(ralign);
+
+	textbox = new JTextField(5);
+
+	editor.add(textbox);
+	editor.add(bold);
+	editor.add(italic);
+	editor.add(lalign);
+	editor.add(center);
+	editor.add(ralign);
+    }
+
+    public void actionPerformed(ActionEvent e){
+	// ____ function = new ___(); name of class that turns text
+	String event = e.getActionCommand();
+	if(event.equals("turnB")){
+	    
+	}
+	if(event.equals("turnI")){
+	    
+    	}
+	if (event.equals("Left-aligned")){
+	    //alignment.setSelected(getSelection(),false);
+	    lalign.setSelected(true);
+	    textbox.setHorizontalAlignment(JTextField.LEFT);
+	}else if (event.equals("Right-aligned")){
+	    //alignment.setSelected(getSelection(),false);
+	    ralign.setSelected(true);
+	    textbox.setHorizontalAlignment(JTextField.RIGHT);
+	}else{
+	    //alignment.setSelected(getSelection(),false);
+	    center.setSelected(true);
+	    textbox.setHorizontalAlignment(JTextField.CENTER);
+	}
+ 
+    }
+
+    public static void main(String[] args){
+	Menu test = new Menu();
+	test.setVisible(true);
+    }
     
     
 }
