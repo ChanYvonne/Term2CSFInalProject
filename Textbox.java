8e5efa4ae32253748408636e6d914708acdbac0b
Yvonne Chan <ychan1352@gmail.com>
2016-05-22 20:32:44 -0400

    now it's not printing what it should print....

--- Textbox.java
+++ Textbox.java
@@ -29,6 +29,8 @@ public class Textbox{
 	    return style;
 	}
 
+	//set methods for later so we can change only certain parts of the text
+
 	public void setText(String character){
 	    text = character;
 	}
@@ -51,31 +53,60 @@ public class Textbox{
     private Node[] text;
     private int size;
 
-    public Textbox{
+    public Textbox(){
 	text = new Node[100];
-	size = 100;
-	for (int x = 0; x < size;x++){
-	    text[x] = new Node("","",0,0);
+	for (int x = 0; x < text.length;x++){
+	    text[x] = new Node("","",14,0);
 	}
     }
 
-    public void grow(){
+    public void add(String word,String font,int size, int style){
+	//System.out.println(text.length);
+	if (size == text.length){
+	  grow();
+	}
+	text[size] = new Node(word,font,size,style);
+	size++;
+    }
+
+    public void undo(){
+	if (size == 0){
+	    throw new NoSuchElementException();
+	}
+	size--;
+	text[size] = null;
+    }
+
+    public void grow(){ //for if the user is wordy
 	Node[] temp = new Node[size*2+1];
-	for (int x = 0; x < data.length; x++){
+	for (int x = 0; x < size; x++){
 	    temp[x] = text[x];
 	}
 	text = temp;
+    }
+
+    public int getSize(){
+	return size;
     }
     
     public String toString(){
 	String ans = "";
 	for (int x = 0; x < size; x++){
-	    ans += text[x].get(0);
+	    ans += text[x].text();
+	    //System.out.println("does it work");
 	}
 	return ans;
     }
 
+    // for some reason when I print test, it doesn't print out the text() of nodes I added
     public static void main(String[] args){
 	Textbox test = new Textbox();
+	test.add("hello","Times New Roman", 14, 0);
+	test.add("my","Times New Roman", 14, 0);
+	test.add("textbox","Times New Roman", 14, 0);
+	test.add("please","Times New Roman", 14, 0);
+	test.add("work","Times New Roman", 14, 0);
+	System.out.println(test.getSize());
+	System.out.println(test);
     }
 }
